6b9c8b9625472716cf575ea0aa42f947e111a719
Maciej Smolinski <msblaze89@gmail.com>
2013-10-14 23:28:10 +0200

    Write documentation.

--- js-spy.js
+++ js-spy.js
@@ -1,4 +1,35 @@
-// Spy Function
+/**
+ * Spies property usage of a given object.
+ *
+ * Works in modern browsers only, these that support:
+ * - Object.defineProperty
+ * - Object.keys
+ * - Array.prototype.forEach
+ *
+ * Be careful with IE8. Even though it supports Object.defineProperty, it's implementation is not valid.
+ *
+ *
+ *
+ * # Sample Usage
+ *
+ *   // Object to be spied:
+ *   var user = { name: 'Maciej', surname: 'Smolinski', fullName: 'Maciej Smolinski' };
+ *
+ *   // Spy it's properties, debug info namespaced with 'user' label:
+ *   spyProperties('user', user);
+ *
+ *   // Access object's properties as usual:
+ *   console.log('Full Name is: ' + user.name + ' ' + user.surname);
+ *
+ *   // Watch debug info in console:
+ *   [Property Usage] user.name
+ *   [Property Usage] user.surname
+ *
+ *
+ * @param  {String} debugNamespace  Debugging label (namespace)
+ * @param  {Object} objectReference Object which properties you want to spy
+ * @return {[type]}                 [description]
+ */
 function spyProperties (debugNamespace, objectReference) {
 
     Object.keys(objectReference).forEach(function (property) {
