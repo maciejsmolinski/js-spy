59d507a5bb4b29971b1fa3ae9ea5a577320eb97d
Maciej Smolinski <msblaze89@gmail.com>
2013-10-14 23:28:26 +0200

    Rescue from IE non-valid Object.defineProperty implementation.

--- js-spy.js
+++ js-spy.js
@@ -32,24 +32,38 @@
  */
 function spyProperties (debugNamespace, objectReference) {
 
-    Object.keys(objectReference).forEach(function (property) {
+  Object.keys(objectReference).forEach(function (property) {
+    try {
 
-      // 1. Store original value as __<propertyName>
+      // Store original value as __<propertyName>
       user['__' + property] = user[property];
-      // 1. Reset property to undefined
+      // Reset property to undefined
       user[property]        = undefined;
 
-      // 2. Define spy (will write debug info into console and return original value)
+      // Define spy (will write debug info into console and return original value)
       Object.defineProperty(user, property, {
         get: function () {
 
-          // 2.1. Write debug info into console
+          // Write debug info into console
           console.debug('[Property Usage] %debugNamespace.%property'.replace('%debugNamespace', debugNamespace).replace('%property', property));
 
-          // 2.2. Return original value
+          // Return original value
           return user['__' + property];
 
         }
       });
-    });
+
+    } catch (error) {
+      // The only workaround for Object.defineProperty problems in IE8
+
+      if (user['__' + property]) {
+        // Restore original value
+        user[property]        = user['__' + property];
+        // Reset __<property> to undefined
+        user['__' + property] = undefined;
+      }
+
+    }
+  });
+
 }
